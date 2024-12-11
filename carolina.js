6642f363f755ede3657a95222992a9e3a8c97805
contaguia <kleguiar@gmail.com>
2024-12-11 12:45:06 -0500

    Add files via upload

---
+++ carolina.js
@@ -0,0 +1,40 @@
+// #resultado {
+//     margin-top: 20px;
+//     font-size: 18px;
+//     color: #333;
+//     padding: 10px;
+//     border: 1px solid #ccc;
+//     border-radius: 5px;
+//     background-color: #fff;
+//     width: 300px;
+//     text-align: center;
+// }
+// #estudiantesGuardados {
+//     position: fixed; /* Fija la posición en la ventana */
+//     bottom: 20px; /* 20px desde el borde inferior de la página */
+//     left: 20px; /* 20px desde el borde izquierdo de la página */
+//     width: 300px; /* Un ancho fijo para que no ocupe todo el ancho */
+//     background-color: #f4f4f4; /* Fondo claro */
+//     padding: 10px;
+//     border-radius: 5px;
+//     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Sombra suave */
+//     overflow-y: auto; /* Si hay muchas tareas, agregará un scroll */
+//     max-height: 400px; /* Altura máxima para evitar que se extienda demasiado */
+//     z-index: 1000; /* Asegura que el contenedor esté por encima de otros elementos */
+// }
+
+// #estudiantesGuardados div {
+//     margin-bottom: 10px; /* Espacio entre cada estudiante */
+// }
+
+// #estudiantesGuardados h3 {
+//     font-size: 18px;
+//     color: #2980b9; /* Color para el nombre */
+//     margin-bottom: 5px;
+// }
+
+// #estudiantesGuardados p {
+//     font-size: 14px;
+//     margin: 5px 0;
+//     color: #555; /* Color gris para las notas y el promedio */
+// }
